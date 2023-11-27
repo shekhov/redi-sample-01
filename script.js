44b4dd5e899d600c16ff602dc7138ba4c6e1a26e
Anton Shekhov <shekhov.anton@gmail.com>
2023-11-27 21:08:16 +0100

    Simple counter functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const decButton = document.getElementById("decrease");
+const incButton = document.getElementById("increase");
+const counterLabel = document.getElementById("counter-label");
+
+function changeCounter (byNumber)
+{
+    counterLabel.innerText = parseInt(counterLabel.innerText) + byNumber;
+}
+
+decButton.onclick = () => {
+    changeCounter(-1);
+};
+
+incButton.onclick = () => {
+    changeCounter(1);
+};
